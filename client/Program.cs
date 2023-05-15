d886295e7cf2cd6e953fbecb9ec0d3333a0a132e
sebastian_stenka <sebastian.stenka@solwena.com>
2023-05-15 08:36:00 +0200

    Grpc Unary

---
+++ client/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Greet;
+using Grpc.Core;
+
+namespace client
+{
+    internal class Program
+    {
+        const string Target = "127.0.0.1:50051";
+        public static void Main(string[] args)
+        {
+            Channel channel = new Channel(Target, ChannelCredentials.Insecure);
+
+            channel.ConnectAsync().ContinueWith((task) =>
+            {
+                if(task.Status == TaskStatus.RanToCompletion)
+                {
+                    Console.WriteLine("Client connected");
+                }
+            });
+
+            var client = new GreetingService.GreetingServiceClient(channel);
+            var request = new GreetingRequest()
+            {
+                Greeting = new Greeting() { FirstName = "John", LastName = "Smith" }
+            };
+
+            var response = client.Greet(request);
+            Console.WriteLine(response.Result);
+
+            channel.ShutdownAsync().Wait();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ server/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using Greet;
+using Grpc.Core;
+
+namespace server
+{
+    internal class Program
+    {
+        const int Port = 50051;
+        public static void Main(string[] args)
+        {
+            Server server = null;
+
+            try
+            {
+                server = new Server
+                {
+                    Services = { GreetingService.BindService(new GreetingServiceImpl()) },
+                    Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+                };
+
+                server.Start();
+                Console.WriteLine($"The server is listening port {Port}");
+                Console.ReadKey();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+            finally
+            {
+                if (server != null)
+                {
+                    server.ShutdownAsync().Wait();
+                }
+            }
+        }
+    }
+}
